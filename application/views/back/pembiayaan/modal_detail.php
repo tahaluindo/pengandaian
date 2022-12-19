fe3f0daf6e507b7d5e2aeb4a7b221f48d4d02830
ridargustia <ridargp@gmail.com>
2022-12-19 15:41:40 +0700

    Membuat fitur detail data pembiayaan dengan modal bootstrap

---
+++ application/views/back/pembiayaan/modal_detail.php
@@ -0,0 +1,102 @@
+<div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="max-width: 70%;" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Detail <?php echo $page_title ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Content -->
+                <div class="table-responsive">
+                    <table class="table align-items-center table-flush">
+                        <tbody>
+                            <tr>
+                                <td width="240px">Nama Anggota</td>
+                                <td width="5px">:</td>
+                                <td><b><span class="name"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>NIK</td>
+                                <td>:</td>
+                                <td><b><span class="nik"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td>
+                                <td>:</td>
+                                <td><b><span class="address"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>:</td>
+                                <td><b><span class="email"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>No. HP/Telephone/WA</td>
+                                <td>:</td>
+                                <td><b>+<span class="phone"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Jumlah Pinjaman</td>
+                                <td>:</td>
+                                <td><b>Rp. <span class="jml_pinjaman"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Jangka Waktu Pinjaman</td>
+                                <td>:</td>
+                                <td><b><span class="jangka_waktu_pinjam"></span> Bulan</b></td>
+                            </tr>
+                            <tr>
+                                <td>Jenis Barang Yang Digadaikan</td>
+                                <td>:</td>
+                                <td><b><span class="jenis_barang_gadai"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Berat Barang Yang Digadaikan</td>
+                                <td>:</td>
+                                <td><b><span class="berat_barang_gadai"></span> Gram</b></td>
+                            </tr>
+                            <tr>
+                                <td>Waktu Gadai</td>
+                                <td>:</td>
+                                <td><b><span class="waktu_gadai"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Jatuh Tempo Gadai</td>
+                                <td>:</td>
+                                <td><b><span class="jatuh_tempo_gadai"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Jangka Waktu Gadai</td>
+                                <td>:</td>
+                                <td><b><span class="jangka_waktu_gadai"></span> Bulan</b></td>
+                            </tr>
+                            <tr>
+                                <td>Sewa Tempat Perbulan</td>
+                                <td>:</td>
+                                <td><b>Rp. <span class="sewa_tempat_perbulan"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Total Biaya Sewa</td>
+                                <td>:</td>
+                                <td><b>Rp. <span class="total_biaya_sewa"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Sistem Pembayaran Sewa</td>
+                                <td>:</td>
+                                <td><b><span class="sistem_pembayaran_sewa"></span></b></td>
+                            </tr>
+                            <tr>
+                                <td>Sumber Dana</td>
+                                <td>:</td>
+                                <td><b><span class="sumber_dana"></span></b></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- Content -->
+            </div>
+        </div>
+    </div>
+</div>
--- application/views/back/pembiayaan/pembiayaan_list.php
+++ application/views/back/pembiayaan/pembiayaan_list.php
@@ -56,15 +56,29 @@
                                         <tbody>
                                             <?php
                                             foreach ($get_all as $data) {
+                                                if ($data->sistem_pembayaran_sewa == 1) {
+                                                    $sistem_pembayaran_sewa = 'Bulanan';
+                                                } elseif ($data->sistem_pembayaran_sewa == 2) {
+                                                    $sistem_pembayaran_sewa = 'Jatuh Tempo';
+                                                }
+
+                                                if ($data->sumber_dana == 1) {
+                                                    $sumber_dana = 'Tabungan';
+                                                } elseif ($data->sumber_dana == 2) {
+                                                    $sumber_dana = 'Deposito';
+                                                } elseif ($data->sumber_dana == 3) {
+                                                    $sumber_dana = 'Tabungan dan Deposito';
+                                                }
+
                                                 // Action
-                                                $edit = '<a href="#" id="editDeposito" class="btn btn-sm btn-warning" title="Edit Data" data-toggle="modal" data-target="#exampleModal" data-id_deposito="' . $data->id_deposito . '" data-name="' . $data->name . '" data-nik="' . $data->nik . '" data-address="' . $data->address . '" data-email="' . $data->email . '" data-phone="' . $data->phone . '" data-total_deposito="' . $data->total_deposito . '" data-waktu_deposito="' . $data->waktu_deposito . '" data-jatuh_tempo="' . $data->jatuh_tempo . '"><i class="fas fa-pen"></i></a>';
-                                                $delete = '<a href="' . base_url('admin/deposito/delete/' . $data->id_deposito) . '" id="delete-button" class="btn btn-sm btn-danger" title="Hapus Data"><i class="fas fa-trash"></i></a>';
-                                                $detail = '<a href="#" id="detailDeposito" class="btn btn-sm btn-info" title="Detail Data" data-toggle="modal" data-target="#detailModal" data-name="' . $data->name . '" data-nik="' . $data->nik . '" data-address="' . $data->address . '" data-email="' . $data->email . '" data-phone="' . $data->phone . '" data-total_deposito="' . number_format($data->total_deposito, 2, ',', '.') . '" data-jangka_waktu="' . $data->jangka_waktu . '" data-waktu_deposito="' . date_indonesian_only($data->waktu_deposito) . '" data-jatuh_tempo="' . date_indonesian_only($data->jatuh_tempo) . '" data-bagi_hasil="' . $data->bagi_hasil . '"><i class="fas fa-info-circle"></i></a>';
+                                                $edit = '<a href="#" id="editPembiayaan" class="btn btn-sm btn-warning" title="Edit Data" data-toggle="modal" data-target="#exampleModal" data-id_pembiayaan="' . $data->id_pembiayaan . '" data-name="' . $data->name . '" data-nik="' . $data->nik . '" data-address="' . $data->address . '" data-email="' . $data->email . '" data-phone="' . $data->phone . '" data-jml_pinjaman="' . $data->jml_pinjaman . '" data-jangka_waktu_pinjam="' . $data->jangka_waktu_pinjam . '" data-jenis_barang_gadai="' . $data->jenis_barang_gadai . '" data-berat_barang_gadai="' . $data->berat_barang_gadai . '" data-waktu_gadai="' . $data->waktu_gadai . '" data-jatuh_tempo_gadai="' . $data->jatuh_tempo_gadai . '" data-sistem_pembayaran_sewa="' . $data->sistem_pembayaran_sewa . '" data-sumber_dana="' . $data->sumber_dana . '"><i class="fas fa-pen"></i></a>';
+                                                $delete = '<a href="' . base_url('admin/pembiayaan/delete/' . $data->id_pembiayaan) . '" id="delete-button" class="btn btn-sm btn-danger" title="Hapus Data"><i class="fas fa-trash"></i></a>';
+                                                $detail = '<a href="#" id="detailPembiayaan" class="btn btn-sm btn-info" title="Detail Data" data-toggle="modal" data-target="#detailModal" data-name="' . $data->name . '" data-nik="' . $data->nik . '" data-address="' . $data->address . '" data-email="' . $data->email . '" data-phone="' . $data->phone . '" data-jml_pinjaman="' . number_format($data->jml_pinjaman, 2, ',', '.') . '" data-jangka_waktu_pinjam="' . $data->jangka_waktu_pinjam . '" data-jenis_barang_gadai="' . $data->jenis_barang_gadai . '" data-berat_barang_gadai="' . $data->berat_barang_gadai . '" data-waktu_gadai="' . date_indonesian_only($data->waktu_gadai) . '" data-jatuh_tempo_gadai="' . date_indonesian_only($data->jatuh_tempo_gadai) . '" data-jangka_waktu_gadai="' . $data->jangka_waktu_gadai . '" data-sewa_tempat_perbulan="' . number_format($data->sewa_tempat_perbulan, 2, ',', '.') . '" data-total_biaya_sewa="' . number_format($data->total_biaya_sewa, 2, ',', '.') . '" data-sistem_pembayaran_sewa="' . $sistem_pembayaran_sewa . '" data-sumber_dana="' . $sumber_dana . '"><i class="fas fa-info-circle"></i></a>';
                                             ?>
                                                 <tr>
                                                     <td><?php echo $data->name ?></td>
                                                     <td><?php echo $data->nik ?></td>
-                                                    <td><?php echo number_format($data->jml_pinjaman, 0, ',', '.') ?></td>
+                                                    <td>Rp. <?php echo number_format($data->jml_pinjaman, 0, ',', '.') ?></td>
                                                     <td><?php echo $data->created_by ?></td>
                                                     <td><?php echo $detail ?> <?php echo $edit ?> <?php echo $delete ?></td>
                                                 </tr>
@@ -83,11 +97,12 @@
                     <!-- Modal Logout -->
 
                     <!-- Modal Edit -->
-                    <?php $this->load->view('back/deposito/modal_edit'); ?>
+                    <?php //$this->load->view('back/pembiayaan/modal_edit');
+                    ?>
                     <!-- Modal Edit -->
 
                     <!-- Modal detail -->
-                    <?php $this->load->view('back/deposito/modal_detail'); ?>
+                    <?php $this->load->view('back/pembiayaan/modal_detail'); ?>
                     <!-- Modal detail -->
                 </div>
                 <!--Container Fluid-->
@@ -153,48 +168,70 @@
         });
 
         $(document).ready(function() {
-            $(document).on('click', '#editDeposito', function() {
-                const id_deposito = $(this).data('id_deposito');
+            $(document).on('click', '#editPembiayaan', function() {
+                const id_pembiayaan = $(this).data('id_pembiayaan');
                 const name = $(this).data('name');
                 const nik = $(this).data('nik');
                 const address = $(this).data('address');
                 const email = $(this).data('email');
                 const phone = $(this).data('phone');
-                const total_deposito = $(this).data('total_deposito');
-                const waktu_deposito = $(this).data('waktu_deposito');
-                const jatuh_tempo = $(this).data('jatuh_tempo');
-                $('#id_deposito').val(id_deposito);
+                const jml_pinjaman = $(this).data('jml_pinjaman');
+                const jangka_waktu_pinjam = $(this).data('jangka_waktu_pinjam');
+                const jenis_barang_gadai = $(this).data('jenis_barang_gadai');
+                const berat_barang_gadai = $(this).data('berat_barang_gadai');
+                const waktu_gadai = $(this).data('waktu_gadai');
+                const jatuh_tempo_gadai = $(this).data('jatuh_tempo_gadai');
+                const sistem_pembayaran_sewa = $(this).data('sistem_pembayaran_sewa');
+                const sumber_dana = $(this).data('sumber_dana');
+                $('#id_pembiayaan').val(id_pembiayaan);
                 $('#name').val(name);
                 $('#nik').val(nik);
                 $('#address').val(address);
                 $('#email').val(email);
                 $('#phone').val(phone);
-                $('#total_deposito').val(total_deposito);
-                $('#waktu_deposito').val(waktu_deposito);
-                $('#jatuh_tempo').val(jatuh_tempo);
+                $('#jml_pinjaman').val(jml_pinjaman);
+                $('#jangka_waktu_pinjam').val(jangka_waktu_pinjam);
+                $('#jenis_barang_gadai').val(jenis_barang_gadai);
+                $('#berat_barang_gadai').val(berat_barang_gadai);
+                $('#waktu_gadai').val(waktu_gadai);
+                $('#jatuh_tempo_gadai').val(jatuh_tempo_gadai);
+                $('#sistem_pembayaran_sewa').val(sistem_pembayaran_sewa);
+                $('#sumber_dana').val(sumber_dana);
             });
 
-            $(document).on('click', '#detailDeposito', function() {
+            $(document).on('click', '#detailPembiayaan', function() {
                 const name = $(this).data('name');
                 const nik = $(this).data('nik');
                 const address = $(this).data('address');
                 const email = $(this).data('email');
                 const phone = $(this).data('phone');
-                const total_deposito = $(this).data('total_deposito');
-                const jangka_waktu = $(this).data('jangka_waktu');
-                const waktu_deposito = $(this).data('waktu_deposito');
-                const jatuh_tempo = $(this).data('jatuh_tempo');
-                const bagi_hasil = $(this).data('bagi_hasil');
+                const jml_pinjaman = $(this).data('jml_pinjaman');
+                const jangka_waktu_pinjam = $(this).data('jangka_waktu_pinjam');
+                const jenis_barang_gadai = $(this).data('jenis_barang_gadai');
+                const berat_barang_gadai = $(this).data('berat_barang_gadai');
+                const waktu_gadai = $(this).data('waktu_gadai');
+                const jatuh_tempo_gadai = $(this).data('jatuh_tempo_gadai');
+                const jangka_waktu_gadai = $(this).data('jangka_waktu_gadai');
+                const sewa_tempat_perbulan = $(this).data('sewa_tempat_perbulan');
+                const total_biaya_sewa = $(this).data('total_biaya_sewa');
+                const sistem_pembayaran_sewa = $(this).data('sistem_pembayaran_sewa');
+                const sumber_dana = $(this).data('sumber_dana');
                 $('.name').text(name);
                 $('.nik').text(nik);
                 $('.address').text(address);
                 $('.email').text(email);
                 $('.phone').text(phone);
-                $('.total_deposito').text(total_deposito);
-                $('.jangka_waktu').text(jangka_waktu);
-                $('.waktu_deposito').text(waktu_deposito);
-                $('.jatuh_tempo').text(jatuh_tempo);
-                $('.bagi_hasil').text(bagi_hasil);
+                $('.jml_pinjaman').text(jml_pinjaman);
+                $('.jangka_waktu_pinjam').text(jangka_waktu_pinjam);
+                $('.jenis_barang_gadai').text(jenis_barang_gadai);
+                $('.berat_barang_gadai').text(berat_barang_gadai);
+                $('.waktu_gadai').text(waktu_gadai);
+                $('.jatuh_tempo_gadai').text(jatuh_tempo_gadai);
+                $('.jangka_waktu_gadai').text(jangka_waktu_gadai);
+                $('.sewa_tempat_perbulan').text(sewa_tempat_perbulan);
+                $('.total_biaya_sewa').text(total_biaya_sewa);
+                $('.sistem_pembayaran_sewa').text(sistem_pembayaran_sewa);
+                $('.sumber_dana').text(sumber_dana);
             })
         });
     </script>
