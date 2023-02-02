ae7a81f713277692cdab79ea07bfdfca16ebd3de
ridargustia <ridargp@gmail.com>
2023-02-02 11:13:43 +0700

    Membuat fungsi update action untuk mengubah data user di database berdasarkan id

--- application/controllers/admin/Auth.php
+++ application/controllers/admin/Auth.php
@@ -39,6 +39,8 @@ class Auth extends CI_Controller
 
     $this->data['get_all'] = $this->Auth_model->get_all();
 
+    $this->data['action']     = 'admin/auth/update_action';
+
     if (is_grandadmin()) {
       $this->data['get_all_combobox_usertype']     = $this->Usertype_model->get_all_combobox();
     } elseif (is_masteradmin()) {
@@ -46,8 +48,6 @@ class Auth extends CI_Controller
     } elseif (is_superadmin()) {
       $this->data['get_all_combobox_usertype']     = $this->Usertype_model->get_all_combobox_for_superadmin();
     }
-
-    $this->data['get_all_data_access'] = $this->Dataaccess_model->get_all();
 
     $this->data['id_users'] = [
       'name'          => 'id_users',
@@ -395,186 +395,28 @@ class Auth extends CI_Controller
     }
   }
 
-  function update($id)
-  {
-    is_update();
-
-    $this->data['user']     = $this->Auth_model->get_by_id($id);
-    $user                   = $this->data['user'];
-
-    if (is_admin() and is_pegawai()) {
-      $this->session->set_flashdata('message', '<div class="alert alert-danger">Anda tidak berhak masuk ke halaman sebelumnya</div>');
-      redirect('admin/dashboard');
-    }
-
-    if (is_masteradmin() && $user->instansi_id != $this->session->instansi_id) {
-      $this->session->set_flashdata('message', '<div class="alert alert-danger">Anda tidak berhak melihat data orang lain</div>');
-      redirect('admin/auth');
-    }
-    if (is_superadmin() && $user->cabang_id != $this->session->cabang_id) {
-      $this->session->set_flashdata('message', '<div class="alert alert-danger">Anda tidak berhak melihat data orang lain</div>');
-      redirect('admin/auth');
-    }
-    if (is_admin() && $user->divisi_id != $this->session->divisi_id) {
-      $this->session->set_flashdata('message', '<div class="alert alert-danger">Anda tidak berhak melihat data orang lain</div>');
-      redirect('admin/auth');
-    }
-
-    if (is_grandadmin()) {
-      $this->data['get_all_combobox_usertype']     = $this->Usertype_model->get_all_combobox();
-      $this->data['get_all_combobox_instansi']     = $this->Instansi_model->get_all_combobox();
-      $this->data['get_all_combobox_cabang']       = $this->Cabang_model->get_all_combobox_update($this->data['user']->instansi_id);
-      $this->data['get_all_combobox_divisi']       = $this->Divisi_model->get_all_combobox_update($this->data['user']->cabang_id);
-    } elseif (is_masteradmin()) {
-      $this->data['get_all_combobox_cabang']       = $this->Cabang_model->get_all_combobox_by_instansi($this->data['user']->instansi_id);
-      $this->data['get_all_combobox_divisi']       = $this->Divisi_model->get_all_combobox_by_cabang($this->data['user']->cabang_id);
-      $this->data['get_all_combobox_usertype']     = $this->Usertype_model->get_all_combobox_by_instansi($this->data['user']->instansi_id);
-    } elseif (is_superadmin()) {
-      $this->data['get_all_combobox_divisi']       = $this->Divisi_model->get_all_combobox_by_cabang($this->session->cabang_id);
-      $this->data['get_all_combobox_usertype']     = $this->Usertype_model->get_all_combobox_by_cabang($this->session->cabang_id);
-    }
-
-    $this->data['get_all_combobox_instansi']      = $this->Instansi_model->get_all_combobox();
-
-    $this->data['get_all_combobox_data_access']   = $this->Dataaccess_model->get_all_combobox();
-    $this->data['get_all_data_access']            = $this->Dataaccess_model->get_all();
-    $this->data['get_all_data_access_old']        = $this->Dataaccess_model->get_all_data_access_old($id);
-
-    $this->data['page_title'] = 'Update Data ' . $this->data['module'];
-    $this->data['action']     = 'admin/auth/update_action';
-
-    if ($this->data['user']) {
-      $this->data['id_users'] = [
-        'name'          => 'id_users',
-        'type'          => 'hidden',
-      ];
-      $this->data['name'] = [
-        'name'          => 'name',
-        'id'            => 'name',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-        'required'      => '',
-      ];
-      $this->data['birthdate'] = [
-        'name'          => 'birthdate',
-        'id'            => 'birthdate',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-      ];
-      $this->data['birthplace'] = [
-        'name'          => 'birthplace',
-        'id'            => 'birthplace',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-      ];
-      $this->data['gender'] = [
-        'name'          => 'gender',
-        'id'            => 'gender',
-        'class'         => 'form-control',
-      ];
-      $this->data['gender_value'] = [
-        '1'             => 'Male',
-        '2'             => 'Female',
-      ];
-      $this->data['address'] = [
-        'name'          => 'address',
-        'id'            => 'address',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-        'rows'           => '3',
-      ];
-      $this->data['phone'] = [
-        'name'          => 'phone',
-        'id'            => 'phone',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-      ];
-      $this->data['email'] = [
-        'name'          => 'email',
-        'id'            => 'email',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-        'required'      => '',
-      ];
-      $this->data['username'] = [
-        'name'          => 'username',
-        'id'            => 'username',
-        'class'         => 'form-control',
-        'autocomplete'  => 'off',
-        'required'      => '',
-      ];
-      $this->data['instansi_id'] = [
-        'name'          => 'instansi_id',
-        'id'            => 'instansi_id',
-        'class'         => 'form-control',
-        'onChange'      => 'tampilCabang()',
-        'required'      => '',
-      ];
-      $this->data['cabang_id'] = [
-        'name'          => 'cabang_id',
-        'id'            => 'cabang_id',
-        'class'         => 'form-control',
-        'onChange'      => 'tampilDivisi()',
-        'required'      => '',
-      ];
-      $this->data['divisi_id'] = [
-        'name'          => 'divisi_id',
-        'id'            => 'divisi_id',
-        'class'         => 'form-control',
-        'required'      => '',
-      ];
-      $this->data['usertype_id'] = [
-        'name'          => 'usertype_id',
-        'id'            => 'usertype_id',
-        'class'         => 'form-control',
-        'required'      => '',
-      ];
-      $this->data['data_access_id'] = [
-        'name'          => 'data_access_id[]',
-        'id'            => 'data_access_id',
-        'class'         => 'form-control select2',
-        'multiple'      => '',
-      ];
-
-
-      $this->load->view('back/auth/user_edit', $this->data);
-    } else {
-      $this->session->set_flashdata('message', '<div class="alert alert-danger">Data tidak ditemukan</div>');
-      redirect('admin/auth');
-    }
-  }
-
   function update_action()
   {
     $this->form_validation->set_rules('name', 'Nama Lengkap', 'trim|required');
-    $this->form_validation->set_rules('phone', 'No. HP', 'trim|is_numeric');
-    $this->form_validation->set_rules('username', 'Username', 'trim|required');
-    $this->form_validation->set_rules('email', 'Email', 'valid_email|required');
-    // $this->form_validation->set_rules('cabang_id', 'Cabang', 'required');
-    $this->form_validation->set_rules('divisi_id', 'Divisi', 'required');
+    $this->form_validation->set_rules('gender', 'Jenis Kelamin', 'required');
+    $this->form_validation->set_rules('phone', 'No. HP/Telephone', 'trim|is_numeric|required');
+    $this->form_validation->set_rules('username', 'Username', 'trim|is_unique[users.username]|required');
+    $this->form_validation->set_rules('email', 'Email', 'valid_email|is_unique[users.email]|required');
     $this->form_validation->set_rules('usertype_id', 'Usertype', 'required');
+    $this->form_validation->set_rules('data_access_id[]', 'Data Access', 'required');
+
     $this->form_validation->set_message('required', '{field} wajib diisi');
     $this->form_validation->set_message('is_numeric', '{field} harus angka');
+    $this->form_validation->set_message('is_unique', '{field} telah ada, silahkan ganti yang lain');
     $this->form_validation->set_message('valid_email', '{field} format email tidak benar');
 
     $this->form_validation->set_error_delimiters('<div class="alert alert-danger">', '</div>');
 
     if ($this->form_validation->run() === FALSE) {
-      $this->update($this->input->post('id_users'));
+      redirect('admin/auth');
     } else {
-      if (is_grandadmin()) {
-        $instansi_id  = $this->input->post('instansi_id');
-        $cabang_id    = $this->input->post('cabang_id');
-        $divisi_id    = $this->input->post('divisi_id');
-      } elseif (is_masteradmin()) {
-        $instansi_id  = $this->session->userdata('instansi_id');
-        $cabang_id    = $this->input->post('cabang_id');
-        $divisi_id    = $this->input->post('divisi_id');
-      } elseif (is_superadmin()) {
-        $instansi_id  = $this->session->userdata('instansi_id');
-        $cabang_id    = $this->session->userdata('cabang_id');
-        $divisi_id    = $this->input->post('divisi_id');
-      }
+      //Format penulisan username
+      $username = str_replace(' ', '', strtolower($this->input->post('username')));
 
       if ($_FILES['photo']['error'] <> 4) {
         $nmfile = strtolower(url_title($this->input->post('username'))) . date('YmdHis');
@@ -600,7 +442,7 @@ class Auth extends CI_Controller
           $error = array('error' => $this->upload->display_errors());
           $this->session->set_flashdata('message', '<div class="alert alert-danger">' . $error['error'] . '</div>');
 
-          $this->update($this->input->post('id_users'));
+          redirect('admin/auth');
         } else {
           $photo = $this->upload->data();
 
@@ -616,18 +458,17 @@ class Auth extends CI_Controller
 
           $data = array(
             'name'              => $this->input->post('name'),
+            'gender'            => $this->input->post('gender'),
             'birthdate'         => $this->input->post('birthdate'),
             'birthplace'        => $this->input->post('birthplace'),
-            'gender'            => $this->input->post('gender'),
             'address'           => $this->input->post('address'),
             'phone'             => $this->input->post('phone'),
             'email'             => $this->input->post('email'),
-            'username'          => strtolower($this->input->post('username')),
-            'instansi_id'       => $instansi_id,
-            'cabang_id'         => $cabang_id,
-            'divisi_id'         => $divisi_id,
+            'username'          => $username,
+            'instansi_id'       => $this->session->instansi_id,
             'usertype_id'       => $this->input->post('usertype_id'),
             'modified_by'       => $this->session->username,
+            'ip_add_reg'        => $this->input->ip_address(),
             'photo'             => $this->upload->data('file_name'),
             'photo_thumb'       => $nmfile . '_thumb' . $this->upload->data('file_ext'),
           );
@@ -642,36 +483,36 @@ class Auth extends CI_Controller
 
             $data_access_id         = count($this->input->post('data_access_id'));
 
-            for ($i_data_access_id = 0; $i_data_access_id < $data_access_id; $i_data_access_id++) {
-              $datas_data_access_id[$i_data_access_id] = array(
+            for ($i = 0; $i < $data_access_id; $i++) {
+              $datas_data_access_id[$i] = array(
                 'user_id'           => $this->input->post('id_users'),
-                'data_access_id'    => $this->input->post('data_access_id[' . $i_data_access_id . ']'),
+                'data_access_id'    => $this->input->post('data_access_id[' . $i . ']'),
               );
 
-              $this->db->insert('users_data_access', $datas_data_access_id[$i_data_access_id]);
+              $this->db->insert('users_data_access', $datas_data_access_id[$i]);
 
               write_log();
             }
           }
 
-          $this->session->set_flashdata('message', '<div class="alert alert-success">Data berhasil disimpan</div>');
+          $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><h6 style="margin-top: 3px; margin-bottom: 3px;"><i class="fas fa-check"></i><b> Data Berhasil Disimpan!</b></h6></div>');
           redirect('admin/auth');
         }
       } else {
         $data = array(
           'name'              => $this->input->post('name'),
+          'gender'            => $this->input->post('gender'),
           'birthdate'         => $this->input->post('birthdate'),
           'birthplace'        => $this->input->post('birthplace'),
-          'gender'            => $this->input->post('gender'),
           'address'           => $this->input->post('address'),
           'phone'             => $this->input->post('phone'),
           'email'             => $this->input->post('email'),
-          'username'          => strtolower($this->input->post('username')),
-          'instansi_id'       => $instansi_id,
-          'cabang_id'         => $cabang_id,
-          'divisi_id'         => $divisi_id,
+          'username'          => $username,
+          'instansi_id'       => $this->session->instansi_id,
           'usertype_id'       => $this->input->post('usertype_id'),
           'modified_by'       => $this->session->username,
+          'ip_add_reg'        => $this->input->ip_address(),
+          'photo'             => 'noimage.jpg',
         );
 
         $this->Auth_model->update($this->input->post('id_users'), $data);
@@ -684,19 +525,19 @@ class Auth extends CI_Controller
 
           $data_access_id = count($this->input->post('data_access_id'));
 
-          for ($i_data_access_id = 0; $i_data_access_id < $data_access_id; $i_data_access_id++) {
-            $datas_data_access_id[$i_data_access_id] = array(
+          for ($i = 0; $i < $data_access_id; $i++) {
+            $datas_data_access_id[$i] = array(
               'user_id'           => $this->input->post('id_users'),
-              'data_access_id'    => $this->input->post('data_access_id[' . $i_data_access_id . ']'),
+              'data_access_id'    => $this->input->post('data_access_id[' . $i . ']'),
             );
 
-            $this->db->insert('users_data_access', $datas_data_access_id[$i_data_access_id]);
+            $this->db->insert('users_data_access', $datas_data_access_id[$i]);
 
             write_log();
           }
         }
 
-        $this->session->set_flashdata('message', '<div class="alert alert-success">Data berhasil disimpan</div>');
+        $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><h6 style="margin-top: 3px; margin-bottom: 3px;"><i class="fas fa-check"></i><b> Data Berhasil Disimpan!</b></h6></div>');
         redirect('admin/auth');
       }
     }
